002207696071cc28b7ae70f7c89efe08bafc30ad
shaileh Bisht <shaileshbisht765@gmail.com>
2022-06-14 01:40:14 +0530

    create routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,29 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Switch } from "react-router-dom";
+import { Actors, MovieInformation, Movies, NavBar, Profile } from "./";
 
-const App = () => <div>App</div>;
+const App = () => (
+  <div>
+    <CssBaseline />
+    <NavBar />
+    <main>
+      <Switch>
+        <Route path="/movie/:id" exact>
+          <MovieInformation />
+        </Route>
+        <Route path="/actors/:id" exact>
+          <Actors />
+        </Route>
+        <Route path="/" exact>
+          <Movies />
+        </Route>
+        <Route path="/profile/:id" exact>
+          <Profile />
+        </Route>
+      </Switch>
+    </main>
+  </div>
+);
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Actors } from "../components/Actors/Actors.jsx";
+export { default as MovieInformation } from "../components/MovieInformation/MovieInformation.jsx";
+export { default as Movies } from "../components/Movies/Movies.jsx";
+export { default as NavBar } from "../components/NavBar/NavBar.jsx";
+export { default as Profile } from "../components/Profile/Profile.jsx";
