a6df006289d29f4aa38c951928d74deb9e533b8c
shaileh Bisht <shaileshbisht765@gmail.com>
2022-06-28 00:17:13 +0530

    single movie details part-1

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import { Box, Typography, Button } from "@mui/material";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+
+import { userSelector } from "../../features/auth";
+import { ExitToApp } from "@mui/icons-material";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const { user } = useSelector(userSelector);
+
+  const favoriteMovies = [];
+
+  // console.log(user);
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = "/";
+  };
+
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies.length ? (
+        <Typography variant="h5">
+          Add Favorites or watchlist some movies to see them here!
+        </Typography>
+      ) : (
+        <Box>FAVORITE MOVIES</Box>
+      )}
+    </Box>
+  );
 };
 
 export default Profile;
