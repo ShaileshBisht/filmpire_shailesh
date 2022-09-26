a16946d92cf80aa5241f4bbf506402ae10e1feb6
shaileh Bisht <shaileshbisht765@gmail.com>
2022-09-26 23:04:01 +0530

    change category/genre using alan ai voice assistant

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,17 +1,40 @@
 import React, { useEffect, useContext } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
+import {
+  selectGenreOrCategory,
+  searchMovie,
+} from "../features/currentGenreOrCategory";
 import { fetchToken } from "../utils";
 import { ColorModeContext } from "../utils/ToggleColorMode";
 
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   useEffect(() => {
     alanBtn({
       key: "7a54e63dc0fe8072bf52256d5d6d71572e956eca572e1d8b807a3e2338fdd0dc/stage",
-      onCommand: ({ command, mode }) => {
-        if (command === "changeMode") {
+      onCommand: ({ command, mode, genres, genreOrCategory }) => {
+        if (command === "chooseGenre") {
+          const foundGenre = genres.find(
+            (g) => g.name.toLowerCase() === genreOrCategory.toLowerCase()
+          );
+
+          if (foundGenre) {
+            history.push("/");
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith("top")
+              ? "top_rated"
+              : genreOrCategory;
+            history.push("/");
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === "changeMode") {
           if (mode === "light") {
             setMode("light");
           } else {
@@ -21,7 +44,7 @@ const useAlan = () => {
           fetchToken();
         } else if (command === "logout") {
           localStorage.clear();
-          window.location.href = "/";
+          history.push("/");
         }
       },
     });
